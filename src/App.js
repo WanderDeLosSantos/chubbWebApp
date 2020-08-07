a7b687a088501307f9222866df1ff530b34da72e
Wander De Los Santos <37256593+WanderDeLosSantos@users.noreply.github.com>
2020-08-07 17:35:16 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Login } from './components'
+import React, { Fragment} from 'react'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
-function App() {
+// Components
+import Main from './components/main/Main'
+import CreateTicket from './components/create-ticket/CreateTicket'
+import Help from './components/help/Help'
+import UserDash from './components/user-dash/UserDash'
+import TicketStatus from './components/ticket-status/TicketStatus'
+
+
+export default () => {
   return (
-    <div className="s">
-     
-          
-          <Login />
-        </div>
-      
+    <Fragment>
+      <Switch>
+      <div className="App">
+        <header className="App-header">
+          <Main/>
+        </header>
+      </div>
+        <Router>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/user-dashboard" component={UserDash} />
+          <Route exact path="/create-ticket" component={CreateTicket} />
+          <Route exact path="/help" component={Help} />
+          <Route exact path="/ticket-status" component={TicketStatus} />
+        </Router>
+      </Switch>
+    </Fragment>
   );
 }
 
-export default App;
